bcdd0e14342891485274283339ca92194248f019
rajat <rajat@niraginfotech.com>
2023-10-03 17:13:15 +0530

    change code

--- doctor/tests.py
+++ doctor/tests.py
@@ -3,77 +3,18 @@ from django.test import TestCase
 # Create your tests here.
 import uuid
 from django.http import JsonResponse
-from django.shortcuts import render
 from twilio.jwt.access_token.grants import VideoGrant
 from twilio.jwt.access_token import AccessToken
-from administrator.models import UserPushNotification
-from rest_framework.decorators import api_view
-from core.mixins import DoctorViewMixin
-from core.decorators import token_required
-from utilities.utils import (
-    is_valid_date,
-    is_valid_email,
-    is_valid_phone,
-)
-
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-import requests
 from django.conf import settings
-from django.db import transaction
-from doctor.models import (  # Doctor Models
-    Users,
-    Patients,
-    Appointments,
-)
-from doctor.serializers import (  # Doctor Serializers
-    AppointmentsSerializer,
-)
-
-
-from utilities.algo import get_available_time_slots
-
-
-# # views.py
-
-# from .models import VideoRoom
-
-# class GenerateVideoAccessToken(APIView):
-#     def get(self, request):
-#         identity = request.query_params.get('identity')
-#         room_name = request.query_params.get('room_name')
-#         # token = AccessToken(account_sid="AC70ea0564259686e89e4a1c58f3bb98e2",
-#         #                     token="dcd4a31c42d209c6df17fc3ebdf539fd",
-#         #                     identity="O4x40EexJbNUnDzwf5iY8rzcUTz5gMnq",
-#         #                     )
-#         token = AccessToken(
-#             "AC70ea0564259686e89e4a1c58f3bb98e2",
-#             "dcd4a31c42d209c6df17fc3ebdf539fd",
-#             "O4x40EexJbNUnDzwf5iY8rzcUTz5gMnq"
-#         )
-#         token.add_grant(VideoGrant(room=room_name))
-#         print(token)
-
-#         return Response({'token': token.to_jwt()})
-
-
-def join_call(request):
-    context = {"identity": "O4x40EexJbNUnDzwf5iY8rzcUTz5gMnq", "roomName": "testing"}
-    return render(request, "join.html", context)
-
 
 def create_video_room(request):
     # Create a unique room name for the video call
-    room_name = f"consult_{uuid.uuid4}"
-    identity = str(uuid.uuid4)
-    # 'O4x40EexJbNUnDzwf5iY8rzcUTz5gMnq'
+    room_name = f"consult_{uuid.uuid4()}"
+    identity = str(uuid.uuid4())
 
-    # Generate a Twilio access token
-    # token = AccessToken(account_sid = "AC70ea0564259686e89e4a1c58f3bb98e2", token = "dcd4a31c42d209c6df17fc3ebdf539fd", identity = identity)
     token = AccessToken(
-        "AC70ea0564259686e89e4a1c58f3bb98e2",  # ACCOUNT_SID
-        "dcd4a31c42d209c6df17fc3ebdf539fd",  # AUTH_TOKEN
+        settings.TWILIO_ACCOUNT_SID,
+        settings.TWILIO_AUTH_TOKEN,
         identity,
         nbf="",
         valid_until="",
@@ -82,41 +23,4 @@ def create_video_room(request):
 
     matting_url = f"http://127.0.0.1:9000/doctor/join_call?identity={identity}&roomName={room_name}"
     # Return the token as a JSON response
-    return JsonResponse({"token": token.to_jwt()})
-
-
-# class ZoomMeetingView(APIView):
-#     def post(self, request, format=None):
-#         topic = request.data.get("topic")
-#         # Format: '2023-07-26T15:00:00'
-#         start_time = request.data.get("start_time")
-#         # duration = request.data.get('duration')  # Duration in minutes
-
-#         if topic and start_time:
-#             headers = {
-#                 "Authorization": f"Bearer {settings.ZOOM_API_TOKEN}",
-#                 "Content-Type": "application/json",
-#             }
-
-#             data = {
-#                 "topic": topic,
-#                 "type": 2,  # Scheduled Meeting
-#                 "start_time": start_time,
-#                 "duration": settings.MEETING_DURATION,
-#                 "timezone": "UTC",  # Adjust as needed
-#             }
-
-#             response = requests.post(
-#                 "https://api.zoom.us/v2/users/me/meetings", json=data, headers=headers
-#             )
-
-#             if response.status_code == 201:
-#                 meeting_data = response.json()
-#                 join_url = meeting_data.get("join_url")
-#                 return Response({"join_url": join_url}, status=201)
-#             else:
-#                 return Response(
-#                     {"error": "Error creating meeting"}, status=response.status_code
-#                 )
-#         else:
-#             return Response({"error": "Missing required data"}, status=400)
+    return JsonResponse({"token": token.to_jwt(), "room_name": room_name})
--- main.py
+++ main.py
@@ -1,88 +1,18 @@
-# import requests
-# import jwt
-# import time
+import os
+import uuid
+from twilio.rest import Client
+from dotenv import load_dotenv
 
-# def generate_jwt_token():
-#     # Replace with your actual client ID and client secret
-#     CLIENT_ID = "dKC9oEqnTbVsDXQSDMtQ"
-#     CLIENT_SECRET = "mHsDnF8wHuaoBvt0ipmVo8ndfcNr0aQR"
+load_dotenv("config.env")
 
-#     # Build the JWT token payload
-#     token_payload = {
-#         "iss": CLIENT_ID,
-#         "exp": int(time.time()) + 3600  # Expiration time (1 hour)
-#     }
+#Twilio
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 
-#     # Generate the JWT token using the client secret
-#     token = jwt.encode(token_payload, CLIENT_SECRET, algorithm='HS256')
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+unique_room_name = "guest_room_" + str(uuid.uuid4())
 
-#     return token
-
-# def create_zoom_meeting():
-#     # Generate the JWT token
-#     access_token = generate_jwt_token()
-
-#     # Zoom API endpoint for creating a meeting
-#     api_url = "https://api.zoom.us/v2/users/me/meetings"
-
-#     # Set headers with the access token
-#     headers = {
-#         "Authorization": f"Bearer {access_token}",
-#         "Content-Type": "application/json"
-#     }
-
-#     # Request payload to create a meeting
-#     payload = {
-#         "topic": "My Zoom Meeting",
-#         "type": 1  # Scheduled meeting
-#         # Add more meeting details as needed
-#     }
-
-#     data = {
-#                 "topic": "topic",
-#                 "type": 2,  # Scheduled Meeting
-#                 "start_time": "2023-07-26T15:00:00", # Format: '2023-07-26T15:00:00'
-#                 "duration": "15",
-#                 "timezone": "UTC"  # Adjust as needed
-#             }
-
-#     # Make a POST request to create the meeting
-#     response = requests.post(api_url, json=payload, headers=headers)
-#     print(response.text)
-
-#     if response.status_code == 201:
-#         meeting_data = response.json()
-#         return meeting_data
-#     else:
-#         print("Failed to create meeting")
-#         return None
-
-# # Call the function to create a Zoom meeting
-# meeting_info = create_zoom_meeting()
-# if meeting_info:
-#     print("Meeting created successfully:", meeting_info)
+room = client.video.v1.rooms.create(type='group', unique_name=unique_room_name)
 
 
-# #--------------------------------------------------------------------------------------
-
-
-# import os
-# import uuid
-# from twilio.rest import Client
-
-# TWILIO_ACCOUNT_SID="AC9c00b562320674207f7afed84d9c3d7b"
-# TWILIO_AUTH_TOKEN="0950c8a3ff5bfdf99aa1c51e2ba92733"
-
-# client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-# unique_room_name = "guest_room_" + str(uuid.uuid4())
-
-# room = client.video.v1.rooms.create(type='group', unique_name=unique_room_name)
-
-
-# print("url ==== ", room.url)
-
-
-# from core.utils import Response
-
-
-# return Response(True, 'djkds', 200, "fsj", {"s":'d'}, "result")
+print("url ==== ", room.url)
