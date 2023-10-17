60c143ac2df9f8e3de7d1dd8924544b047c4f542
rajat <rajat@niraginfotech.com>
2023-10-17 15:25:34 +0530

    alter twilio token api

--- doctor/twilio_controller.py
+++ doctor/twilio_controller.py
@@ -3,7 +3,10 @@ from django.http import JsonResponse
 from utilities.video.auth import get_access_token
 
 def create_video_room(request):
-    room_name = f"consult_{uuid.uuid4()}"
+    room_name = request.GET.get("room_name")
+    if not room_name:
+        return JsonResponse({"status": False, "message": "Room name not found"})
+    # room_name = f"consult_{uuid.uuid4()}"
     identity = str(uuid.uuid4())
     token = get_access_token(identity=identity, room_name=room_name)
 
