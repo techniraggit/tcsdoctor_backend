f2ff2b7e00deb02750d3a28c821e71b3a4779e1a
rajat <rajat@niraginfotech.com>
2023-10-27 18:21:27 +0530

    prescription email

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from administrator.models import *
 
 # Register your models here.
+admin.site.register(PushNotification)
+admin.site.register(UserPushNotification)
