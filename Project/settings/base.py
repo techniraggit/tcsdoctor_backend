508dfff18086d88994acf93466ab767cf944bade
Rajat <rajat@niraginfotech.com>
2023-11-14 16:53:59 +0530

    try to fix staticfiles issue on server

--- Project/settings/base.py
+++ Project/settings/base.py
@@ -144,9 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-print("STATIC_ROOT === ", STATIC_ROOT)
 
 # Media files
 
