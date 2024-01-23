eb4bc42efa0c35ffdf5889c30c5fd339844d793a
Rajat <rajat@niraginfotech.com>
2024-01-23 16:24:43 +0530

    fix staticfiles issue

--- Project/urls.py
+++ Project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("backend/", admin.site.urls),
@@ -32,6 +33,12 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    urlpatterns += (
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+        )
 
 
 admin.site.site_header = "The Database Captain"
