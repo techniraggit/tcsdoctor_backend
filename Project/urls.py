b6a9f002795f922d1fb09f9e97da0408e4af2a39
Rajat <rajat@niraginfotech.com>
2024-01-23 16:28:00 +0530

    fix staticfiles issue

--- Project/urls.py
+++ Project/urls.py
@@ -31,10 +31,12 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    print("WORKING IN DEBUG MODE")
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 else:
+    print("WORKING IN PRODUCTION MODE")
     urlpatterns += (
         re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
         re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
